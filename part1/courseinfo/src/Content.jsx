ad19ec24f4c54b7879d319b46f2038a5e7d574e8
Billy Stewart <stewartb774@gmail.com>
2025-05-16 12:23:48 -0400

    add Part component for 1.2

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,9 +1,11 @@
+import Part from "./Part"
+
 const Content = (props) => {
     return (
         <div>
-            <p>{props.exercisesPerPart[0].part} {props.exercisesPerPart[0].exercises}</p>
-            <p>{props.exercisesPerPart[1].part} {props.exercisesPerPart[1].exercises}</p>
-            <p>{props.exercisesPerPart[2].part} {props.exercisesPerPart[2].exercises}</p>
+            <Part name={props.exercisesPerPart[0].part} exercises={props.exercisesPerPart[0].exercises} />
+            <Part name={props.exercisesPerPart[1].part} exercises={props.exercisesPerPart[1].exercises} />
+            <Part name={props.exercisesPerPart[2].part} exercises={props.exercisesPerPart[2].exercises} />
         </div>
     )
 }
