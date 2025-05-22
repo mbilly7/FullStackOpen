d88e9923e47233043c762e486f81c2e6def15f70
Billy Stewart <stewartb774@gmail.com>
2025-05-22 13:48:21 -0400

    add total, average, and positive statistics

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -1,9 +1,12 @@
 const Statistics = ({ good, neutral, bad }) => {
     return (
         <div>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
+            <div>good {good}</div>
+            <div>neutral {neutral}</div>
+            <div>bad {bad}</div>
+            <div>all {good + neutral + bad}</div>
+            <div>average {((good * 1) + (neutral * 0) + (bad * -1)) / (good + neutral + bad)}</div>
+            <div>positive {good / (good + neutral + bad) * 100} %</div>
         </div>
     )
 }
