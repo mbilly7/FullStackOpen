88ab9925e85476b0b127f498c89df80d250e08cd
Billy Stewart <stewartb774@gmail.com>
2025-05-23 14:44:51 -0400

    move statistics into html table

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -10,14 +10,16 @@ const Statistics = ({ good, neutral, bad }) => {
     }
 
     return (
-        <div>
-            <StatisticsLine text="good" value={good} />
-            <StatisticsLine text="neutral" value={neutral} />
-            <StatisticsLine text="bad" value={bad} />
-            <StatisticsLine text="all" value={good + neutral + bad} />
-            <StatisticsLine text="average" value={((good * 1) + (neutral * 0) + (bad * -1)) / (good + neutral + bad)} />
-            <StatisticsLine text="positive" value={good / (good + neutral + bad) * 100} />
-        </div>
+        <table>
+            <tbody>
+                <StatisticsLine text="good" value={good} />
+                <StatisticsLine text="neutral" value={neutral} />
+                <StatisticsLine text="bad" value={bad} />
+                <StatisticsLine text="all" value={good + neutral + bad} />
+                <StatisticsLine text="average" value={((good * 1) + (neutral * 0) + (bad * -1)) / (good + neutral + bad)} />
+                <StatisticsLine text="positive" value={good / (good + neutral + bad) * 100 + " %"} />
+            </tbody>
+        </table>
     )
 }
 
