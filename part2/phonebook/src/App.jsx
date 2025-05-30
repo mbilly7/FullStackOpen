ec1d48e47d3fb20ea1f2f6b0d087498f1d86eb1d
Billy Stewart <stewartb774@gmail.com>
2025-05-30 15:29:17 -0400

    add filter feature and set value attribute on input fields

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,16 +2,21 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id: persons.length + 1
     }
     
     if (persons.some(person => person.name === newName)) {
@@ -21,28 +26,35 @@ const App = () => {
 
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={event => setFilter(event.target.value)} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input onChange={event => setNewName(event.target.value)} />
+          name: <input value={newName} onChange={event => setNewName(event.target.value)} />
         </div>
         <div>
-          number: <input onChange={event => setNewNumber(event.target.value)} />
+          number: <input value={newNumber} onChange={event => setNewNumber(event.target.value)} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => 
-        <div key={person.name}>
+      {persons.filter(person => 
+        person.name.toLowerCase().includes(filter.toLowerCase())
+      ).map(person => 
+        <div key={person.id}>
           {person.name} {person.number}
         </div>
-      )}
+      )}  
     </div>
   )
 }
